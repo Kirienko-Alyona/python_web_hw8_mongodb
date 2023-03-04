21a7e442718c53fc423862f4024b1704f6a82c57
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-03-04 06:04:45 +0200

    changes

--- hw_8/hw_8/search.py
+++ hw_8/hw_8/search.py
@@ -1,19 +1,37 @@
+from bson import ObjectId
 from pymongo import MongoClient
 
 from models import Quote, Author
 from connect import conn
 
 
-def search_name(data: str):
-    authors = Author.objects(fullname=data)
-    
-    return res
+def search_name(data: str) -> str:
+    author = Author.objects(fullname=data)
+    for item in author:
+        author_id = item.id
+        search_quote(author_id)
+    return 
 
-
-def search_tag(datas: str):
+def search_quote(author_id: ObjectId) -> list:
     res = []
-    for data in datas:
-        quotes = Quote.objects(tags=data)
+  
+    quotes = Quote.objects(author=author_id)      
+    for quote in quotes:
+        quote = quote.quote
+        if not quote in res:
+            res.append(quote)
+    print(res)
+    return res
+    
+        
+def search_tag(data: str) -> list:
+    res = []
+    try:
+        tags = data.split(",")
+    except:
+        return "This tag isn't."
+    for tag in tags:    
+        quotes = Quote.objects(tags=tag)      
         for quote in quotes:
             quote = quote.quote
             if not quote in res:
@@ -26,6 +44,7 @@ if __name__ == '__main__':
 
     client = MongoClient(conn)
     db = client.mongo_db
-    search_tag(("world", "change"))
-    search_name(("world", "change"))
+    #search_tag("life,live")
+    #search_tag("life")
+    search_name(("Albert Einstein"))
     client.close()
